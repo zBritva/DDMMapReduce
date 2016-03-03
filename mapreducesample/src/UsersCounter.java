f59df83dbcf3b734136856e0f1ee0984f0005278
zBritva <zbritva@gmail.com>
2016-03-03 01:30:32 +0100

    update jar. join example

---
+++ mapreducesample/src/UsersCounter.java
@@ -0,0 +1,98 @@
+/**
+ * Created by zbritva on 02.03.16.
+ */
+
+import org.apache.commons.logging.Log;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.mapred.lib.MultipleInputs;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+public class UsersCounter {
+
+    public static class LogMap extends MapReduceBase implements Mapper<Text, Text, Text, IntWritable> {
+        private String ip, user = new String();
+        private IntWritable one = new IntWritable(1);
+
+        public void map(Text key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+            String line = value.toString();
+            String[] data = line.split("\t");
+
+            this.ip = data[0].trim();
+            this.user = data[1].trim();
+            output.collect(new Text(this.ip), this.one);
+        }
+    }
+
+    public static class CountryMap extends MapReduceBase implements Mapper<LongWritable, Text, Text, Text> {
+        private String ip, country = new String();
+
+        public void map(LongWritable key, Text value, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
+            String line = value.toString();
+            String[] data = line.split("\t");
+
+            this.ip = data[0].trim();
+            this.country = data[1].trim();
+            output.collect(new Text(this.ip), new Text(this.country));
+        }
+    }
+
+    public static class Reduce extends MapReduceBase implements Reducer<Text, Text, Text, Text> {
+        String merge = "";
+        public void reduce(Text key, Iterator<Text> values, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
+            int i = 0;
+            while (values.hasNext()) {
+                if (i == 0) {
+                    merge = values.next().toString() + ",";
+                } else {
+                    merge += values.next().toString();
+                }
+                i++;
+            }
+            output.collect(key, new Text(merge));
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration c=new Configuration();
+        String[] files=new GenericOptionsParser(c,args).getRemainingArgs();
+        Path p1=new Path(files[0]);
+        Path p2=new Path(files[1]);
+        Path p3=new Path(files[2]);
+        FileSystem fs = FileSystem.get(c);
+        if(fs.exists(p3)){
+            fs.delete(p3, true);
+        }
+
+        JobConf conf = new JobConf(c, UsersCounter.class);
+        conf.setJobName("user counter");
+
+        conf.setOutputKeyClass(Text.class);
+        conf.setOutputValueClass(IntWritable.class);
+
+
+        MultipleInputs.addInputPath(conf, p1, TextInputFormat.class, CountryMap.class);
+        MultipleInputs.addInputPath(conf,p2, TextInputFormat.class, LogMap.class);
+        //conf.setMapperClass(Map.class);
+        //conf.setCombinerClass(WCS.Reduce.class);
+        conf.setReducerClass(Reduce.class);
+
+        //conf.setInputFormat(TextInputFormat.class);
+        conf.setOutputFormat(TextOutputFormat.class);
+
+        //FileInputFormat.setInputPaths(conf, new Path(args[0]));
+        FileOutputFormat.setOutputPath(conf, p3);
+
+
+        JobClient.runJob(conf);
+    }
+}
